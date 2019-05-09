911deed20e50bb9481cd867dea0496afc199f6af
Aditya Sriram <adsriram@ucdavis.edu>
2019-05-09 21:52:33 +0000

    committing changes to app

--- Rice_Trait_Analysis/app.R
+++ Rice_Trait_Analysis/app.R
@@ -10,6 +10,8 @@
 library(shiny)
 library(ggplot2)
 
+Ricedata <- read.csv("~/Assignments/Assignment_4_Sriram.Aditya/RiceSNPData/RiceDiversity.44K.MSU6.Phenotypes.csv", na=c("NA","00"))
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
@@ -17,35 +19,53 @@ ui <- fluidPage(
    titlePanel("Rice Trait Analysis"),
    
    #Some helpful information
-   helpText("This application creates a scatter plot of two rice traits, with color corresponding to a third trait that helps")
+   helpText("This application creates a scatter plot of two rice traits, with color corresponding to a third trait that helps differentiate the existing two traits."),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         selectInput("trait1",
+                     "Select your x-axis trait to display:",
+                     c("Amylose.content",
+                     "Protein.content",
+                     "Region",
+                     "Seed.length",
+                     "Alu.Tol")
       ),
+         selectInput("trait2",
+                   "Select your y-axis trait to display:",
+                     c("Amylose.content",
+                     "Protein.content",
+                     "Region",
+                     "Seed.length",
+                     "Alu.Tol")
+      ),
+         selectInput("trait3",
+                   "Select fill trait to compare by:",
+                     c("Amylose.content",
+                      "Protein.content",
+                      "Region",
+                      "Seed.length",
+                      "Alu.Tol")
+   )),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+         plotOutput("scatterPlot")
+      )))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+   output$scatterPlot <- renderPlot({
+      # set up scatter plot
+      p1    <- ggplot(data=Ricedata,
+                      aes_string(x=input$trait1,
+                                 y=input$trait2, 
+                                 color=input$trait3)) 
       
       # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      p1 + geom_point()
    })
 }
 
